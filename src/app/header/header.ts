7f9ebdf288d29642eabb34abbea20b057c79f56d
Hosna <h.m.hosna@gmail.com>
2025-08-21 21:07:29 +0330

    complete user-input components and a custom event added to calculate the inputs

---
+++ src/app/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {}
